2afa640d09eb17f281a556a700d4069c421c3783
Zach McCleaf <mccleaf324@gmail.com>
2015-03-17 18:03:29 -0400

    fixing jQuery on scroll to top feature and hiding it in mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	$(window).on("scroll touchmove", function () {
 	    $('header').toggleClass('shrink', $(document).scrollTop() > 250);
-	});
-	$(".aboutphoto").slideDown();
   });  
 });
 
